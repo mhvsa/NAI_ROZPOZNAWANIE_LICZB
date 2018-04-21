582a489c9f7711ad512aaabbfaf6549abbcddaf3
mhvsa <michal.sabiniarz@gmail.com>
2018-04-21 19:15:58 +0200

    two input done

--- neural_network.py
+++ neural_network.py
@@ -1,15 +1,18 @@
 from numpy import matrix
+from inputs import zeros, ones, twos
+
+zeros_input = zeros.get_zeros_as_input()
+ones_input = ones.get_ones_as_input()
+twos_input = twos.get_twos_as_input()
+
 
 class element_ciagu_treningowego:
     X: matrix
     D: matrix
 
+
 ciag_treningowy = []
 
-# zero = matrix([[0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0]]).transpose()
-
-from inputs import zeros as z
-
-zeros = z.get_zeros_as_input()
-
-# zeros.append([0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0]) #
+for zero in zeros_input:
+    X = matrix(zero)
+    D = matrix([[1], [0], [0]])
